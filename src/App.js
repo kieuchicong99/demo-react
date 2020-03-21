c390cd455551d08957bd3e1cd5c19439bd457d38
kieuchicong99 <kieuchicong99@gmail.com>
2020-03-21 23:34:56 +0700

    example commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello World, My name is ckc
         </p>
         <a
           className="App-link"
